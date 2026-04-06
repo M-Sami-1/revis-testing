6444ef2c2439f42a32ab6381aa0ef33d72338f51
M-Sami-1 <miansamibaba857@gmail.com>
2026-04-06 12:07:56 +0500

    Create wreck1.py

---
+++ wreck1.py
@@ -0,0 +1,44 @@
+# worst python code ever written
+
+import maths   # wrong module name
+
+def add_numbers(a b):   # missing comma
+print("Adding numbers")   # bad indentation
+    return a + b + c   # c is undefined
+
+x = input("Enter number: ")   # string input not converted
+y == 10   # comparison instead of assignment
+
+if x = y:   # assignment inside condition (syntax error)
+    print("Equal"
+else:
+    print("Not Equal"))
+
+for i in range(10)
+    print(i)   # missing colon above
+
+list = [1,2,3]
+print(list[5])   # index out of range
+
+dict = {"a":1, "b":2}
+print(dict["c"])   # key error
+
+while True
+    breakk   # typo in keyword
+
+class MyClass
+    def __init__(self):
+        self.value = 10
+
+obj = MyClass()
+print(obj.val)   # wrong attribute name
+
+def func():
+    return
+    print("This will never run")
+
+func(10)   # function takes no arguments
+
+open("file.txt", "r")   # file may not exist, no handling
+
+print(unknown_var)   # completely undefined variable
