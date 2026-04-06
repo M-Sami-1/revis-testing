cb85e662a67f0f483a379e2a64f3b17c23bf9669
M-Sami-1 <miansamibaba857@gmail.com>
2026-04-06 12:17:57 +0500

    Update ct.cpp

--- ct.cpp
+++ ct.cpp
@@ -1,4 +1,4 @@
 #include <stdio.h>
 Change the `main` function signature to `int main() {`.
-  print("hello")
+Replace `print("hello")` with `printf("hello\n");` to use the standard library function and include a newline.
 }
