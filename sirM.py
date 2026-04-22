cc77a668763511d84f4708d237ddac3d02c3c8af
Muhammad Sami <miansamibaba857@gmail.com>
2026-04-22 15:28:43 +0500

    Create sirM.py

---
+++ sirM.py
@@ -0,0 +1,12 @@
+def calculate_average(numbers)
+    total = 0
+    for num in numbers
+        total = total + num
+    
+    average = total / len(numbers)
+    return average
+
+nums = [10, 20, 30, 40]
+result = calculate_average(nums)
+
+print("Average is: " + result)
